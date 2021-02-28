b045e1e575515a7fcee1f3f59c37be9a3fcbfa9c
Andrey Gritsenko <56474337+gritsenkoandrey@users.noreply.github.com>
2021-02-28 19:33:54 +0300

    End Game

---
+++ Assets/Exit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    private Button _exit;
+
+
+    private void Awake()
+    {
+        _exit.GetComponent<Button>();
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
